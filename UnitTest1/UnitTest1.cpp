e6601ba00bfc4088206d021b4102310b2ac74d8f
Yura <urijkisil2007@gmail.com>
2024-10-13 17:52:18 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6_2_rek/lab_6_2_rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestMax)
+        {
+            int a[10] = { -10, 0, 5, 20, 15, 100, 60, 30, 25, 10 };
+            int expected_max = 100;
+            int actual_max = Max(a, 10, 1, a[0]);
+
+            Assert::AreEqual(expected_max, actual_max);
+        }
+
+        TEST_METHOD(TestMin)
+        {
+            int a[10] = { -10, 0, 5, 20, 15, 100, 60, 30, 25, 10 };
+            int expected_min = -10;
+            int actual_min = Min(a, 10, 1, a[0]);
+            Assert::AreEqual(expected_min, actual_min);
+        }
+	};
+}
---
+++ lab_6_2_rek/lab_6_2_rek.cpp
@@ -0,0 +1,66 @@
+﻿// lab_6_2_rek.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+// Функція для генерації масиву
+void Create(int* a, const int size, const int Low, const int High, int i) {
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+// Функція для виведення масиву
+void Print(const int* a, const int size, int i) {
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+// Функція для максимального значення
+int Max(int* a, const int size, int i, int max)
+{
+    if (a[i] > max)
+        max = a[i];
+    if (i < size - 1)
+        return Max(a, size, i + 1, max);
+    else
+        return max;
+}
+// Функція для мінімального значення
+int Min(int* a, const int size, int i, int min)
+{
+    if (a[i] < min)
+        min = a[i];
+    if (i < size - 1)
+        return Min(a, size, i + 1, min);
+    else
+        return min;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    const int size = 10;
+    int a[size];
+    const int Low = -10;
+    const int High = 100;
+
+    // Генерація масиву
+    Create(a, size, Low, High, 0);
+    cout << "massif a:" << endl;
+    Print(a, size, 0);
+
+    cout << "max = " << Max(a, size, 1, a[0]) << endl;
+    cout << "min = " << Min(a, size, 1, a[0]) << endl;
+
+
+    cin.get();
+    return 0;
+}
+
